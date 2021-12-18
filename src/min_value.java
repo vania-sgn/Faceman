0529b85ed0020db140fa2106751591d04391a0a6
sklad <Skladchikov.vania@gmail.com>
2021-12-19 00:48:26 +0300

    min_value

---
+++ src/min_value.java
@@ -0,0 +1,16 @@
+public class min_value {
+    public static void main(String[] args) {
+        int[] array= new int[] {12, 64,45,34,12,0,12,8};
+        int minValue = array[0];
+        int minIndex = 0;
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i]<minValue){
+                minValue=array[i];
+                minIndex = i;
+
+            }
+            
+        }
+        System.out.printf("minValue "+ minValue +  "\nindex "+minIndex);
+    }
+}
