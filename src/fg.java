d2fcc432623fd92673459c35afefbf9b927c8be9
sklad <Skladchikov.vania@gmail.com>
2022-01-12 15:08:44 +0300

    min_value

--- src/fg.java
+++ src/fg.java
@@ -1,5 +1,21 @@
 public class fg {
     public static void main(String[] args) {
-        System.out.print("dfjdfv");
+        System.out.print("Переменные и типы данных");
+
+        int age = 56;
+        //byte  -128 to 127
+        //short -32768 to 32767 2 занимает 2 байта
+        // int     занимает 4 байта
+        // long -9000000000000    занимает 8 байта
+
+        float f= 4.3422f;
+        double d = 4.543;
+        char fac = 'f' ;
+        String ff= "user_name";
+        boolean bol = true;
+
+        char A='A',B='B',C='C';
+
+
     }
 }
