ac026052aebe176089a1ba869cb15c80ad7f896a
sklad <Skladchikov.vania@gmail.com>
2022-01-18 18:37:59 +0300

    min_value

---
+++ src/Data_user.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Data_user {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+      /*  System.out.println("What is your name ? - ");
+        String name = scan.nextLine();
+        System.out.println("My name is " + name );
+        int num1= scan.nextInt();
+*/
+        float num1=50, num2= 33;
+        float res = num1 % num2;
+        System.out.print(res);
+
+
+
+    }
+}
