b8213b4c97d99672b7841c9d78c21a14a0a40941
sklad <Skladchikov.vania@gmail.com>
2022-01-19 23:21:09 +0300

    min_value

---
+++ src/if_case.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class if_case {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите роль: ");
+        String role = scanner.nextLine();
+        System.out.print("Введите пароль : ");
+        String pass = scanner.nextLine();
+        if ( role.equals("Admin") ){
+            System.out.print("У Вас есть доступ к базе");
+        }
+        else{
+            System.out.print("Вы не имеете доступа админа, как Вас зовут?");
+            String name = scanner.nextLine();
+        }
+    }
+}
