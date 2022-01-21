58e716958b32f03989732b7d8981c47fe6f48525
sklad <Skladchikov.vania@gmail.com>
2022-01-21 15:34:49 +0300

    for_while

---
+++ src/for_while.java
@@ -0,0 +1,19 @@
+public class for_while {
+    public static void main(String[] args) {
+        //for (float i = 100; i > 0 ; i/=2) {
+          //  System.out.println("Element   " + i);
+        //}
+        // int i=1;
+        //while (i<=10){
+        //  System.out.println("Element "+i );
+         //   i++;
+        //}
+
+        int i = 0;
+        do {
+            System.out.println("Element "+i );
+            i++;
+        }while (i<10);
+
+    }
+}
