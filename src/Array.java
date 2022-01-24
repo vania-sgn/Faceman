302796b659aedb91c5120ba163b96f640b99159d
sklad <Skladchikov.vania@gmail.com>
2022-01-24 16:48:21 +0300

    сумма мин значений массива

---
+++ src/Array.java
@@ -0,0 +1,29 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < arr.length ; i++) {
+
+            System.out.print("Введите елемент массива: ");
+            int value = scanner.nextInt();
+            arr[i] = value;
+        }
+        System.out.println("Наш массив ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        int min = arr[0];
+        for (int i = 0; i < arr.length ; i++) {
+            if(min>arr[i]){
+                min = arr[i];
+                System.out.print("MIN Value- " );
+                System.out.print(arr[i]);
+            }
+
+        }
+    }
+}
---
+++ src/arr_min.java
@@ -0,0 +1,15 @@
+public class arr_min {
+    public static void main(String[] args) {
+        int[] a = new int[] {-14, 23, 9, -4, 12, -1};
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 0)
+                sum += a[i];
+        }
+        if (sum == 0)
+            System.out.println("Нет цифер меньше 0");
+        else
+            System.out.println("sum = " + sum);
+    }
+
+}
