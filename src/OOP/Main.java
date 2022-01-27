8020ad2c17b8ab62cee6a1846bf36227748b40c2
sklad <Skladchikov.vania@gmail.com>
2022-01-27 11:45:49 +0300

    the change

---
+++ src/OOP/Main.java
@@ -0,0 +1,6 @@
+package OOP;
+
+public class Main {
+    Transport bmw = new Transport();
+
+}
---
+++ src/OOP/Transport.java
@@ -0,0 +1,8 @@
+package OOP;
+
+public class Transport {
+    public float speed;
+    public int weight;
+    public String color;
+    public byte[] coordinate;
+}
