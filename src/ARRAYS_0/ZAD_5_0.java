c21ae1f9c229ab20745efa2e269d10a9117ba33d
sklad <Skladchikov.vania@gmail.com>
2022-08-11 23:53:59 +0300

    the change

--- src/ARRAYS_0/ZAD_5_0.java
+++ src/ARRAYS_0/ZAD_5_0.java
@@ -2,11 +2,9 @@ package ARRAYS_0;
 
 import java.util.Scanner;
 
-public class ZAD_3_1 {
+public class ZAD_5_0 {
     public static void main (String[] args) {
-        int MAXVALUE = 5;
-        int sum = 0;
-        int count = 0;
+
         int a, b;
 
         Scanner in = new Scanner(System.in);
@@ -25,21 +23,31 @@ public class ZAD_3_1 {
                 arr[i][j] = in.nextInt();
             }
         }
-        in.close();
-
+        System.out.println("Введенный массив:");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + "\t");
-                if (arr[i][j] > MAXVALUE) {
-                    sum += arr[i][j];
-                    count++;
-                }
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i][0];
+            int max = arr[i][0];
 
-        System.out.println("Сума елементів, які більше " + MAXVALUE + " складає: " + sum +
-                ". Їхня кількість дорівнює: " + count);
 
+                for (int j = 0; j < arr[0].length; j++) {
+                    if (min > arr[i][j]) {
+                        min = arr[i][j];
+                    }
+                    if (max < arr[i][j]) {
+                        max = arr[i][j];
+                    }
+
+
+                }
+                System.out.print("Максимальный элемент " + max + " Минимальный элемент " + min + " \n\n");
+
+            }
+        }
     }
-}
+
